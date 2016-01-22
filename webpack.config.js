d1603212b6adab2197d11a726be7ed61a6dec02a
cqh <hzchenqinhui@corp.netease.com>
2016-01-22 16:21:21 +0800

    去除多余注释

--- webpack.config.js
+++ webpack.config.js
@@ -59,21 +59,21 @@ module.exports = {
     }),
     new HtmlWebpackPlugin({
       filename: 'page1.html',
-      template: './ftl/page1.ftl',// Load a custom template
-      inject:true,    //允许插件修改哪些内容，包括head与body
-      hash:true,    //为静态资源生成hash值
-      minify:{    //压缩HTML文件
-        removeComments:true    //移除HTML中的注释
+      template: './ftl/page1.ftl',
+      inject:true,
+      hash:true,
+      minify:{
+        removeComments:true
       },
       chunks: [ 'page1']
     }),
     new HtmlWebpackPlugin({
       filename: 'page2.html',
-      template: './ftl/page2.ftl',// Load a custom template
-      inject:true,    //允许插件修改哪些内容，包括head与body
-      hash:true,    //为静态资源生成hash值
-      minify:{    //压缩HTML文件
-        removeComments:true   //移除HTML中的注释
+      template: './ftl/page2.ftl',
+      inject:true,
+      hash:true,
+      minify:{
+        removeComments:true
       },
       chunks: [ 'page2']
     }),
