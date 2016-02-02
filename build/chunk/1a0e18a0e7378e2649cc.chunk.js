452e770f3fa3ca7a2832da34f46258d634d9d115
cqh <hzchenqinhui@corp.netease.com>
2016-02-02 22:24:48 +0800

    build

---
+++ build/chunk/1a0e18a0e7378e2649cc.chunk.js
@@ -0,0 +1,20 @@
+webpackJsonp([4],{
+
+/***/ 295:
+/***/ function(module, exports, __webpack_require__) {
+
+	var __WEBPACK_AMD_DEFINE_ARRAY__, __WEBPACK_AMD_DEFINE_RESULT__;"use strict";
+	
+	//alert(1)
+	!(__WEBPACK_AMD_DEFINE_ARRAY__ = [__webpack_require__(126)], __WEBPACK_AMD_DEFINE_RESULT__ = function ($) {
+	    //alert(2)
+	    function changeColor() {
+	        $(".outter h1").css("color", "red");
+	    }
+	    return changeColor;
+	}.apply(exports, __WEBPACK_AMD_DEFINE_ARRAY__), __WEBPACK_AMD_DEFINE_RESULT__ !== undefined && (module.exports = __WEBPACK_AMD_DEFINE_RESULT__));
+
+/***/ }
+
+});
+//# sourceMappingURL=1a0e18a0e7378e2649cc.chunk.js.map
---
+++ build/chunk/f7efdfe45fb79d1ca2da.chunk.js
@@ -0,0 +1,19 @@
+webpackJsonp([3],{
+
+/***/ 294:
+/***/ function(module, exports, __webpack_require__) {
+
+	"use strict";
+	
+	var $ = __webpack_require__(126);
+	//alert(1)
+	function changeColor() {
+	    $(".outter h1").css("color", "red");
+	}
+	
+	module.exports = changeColor;
+
+/***/ }
+
+});
+//# sourceMappingURL=f7efdfe45fb79d1ca2da.chunk.js.map
